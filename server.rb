69a8599d9856f9cf9417f741d62b91658cb4452e
Deanna Green <deanna@16vmini.co.uk>
2019-04-01 11:20:29 +0100

    first server, you can interact with it and it will keep asking you the question until your response is quit

---
+++ server.rb
@@ -0,0 +1,17 @@
+require 'socket'
+
+server = TCPServer.new(2345)
+
+socket = server.accept
+
+loop do
+  socket.puts "What do you say?"
+
+  they_said = socket.gets.chomp
+
+  if they_said == 'quit'
+    socket.close
+  else
+    socket.puts "You said: #{they_said}. Goodbye!"
+  end
+end
